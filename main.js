d6f03bdc79fc3fc8d51e4c2900202080397ff277
Tena99 <stacyvolvis@gmail.com>
2022-12-18 22:17:04 +0200

    The seventh task done.

--- main.js
+++ main.js
@@ -1,10 +1,38 @@
-// Task 6.1.
-// Output the multiplication table.
-// You can use - document.write('text');
+// Task 7.
+// Change the first characters of words in the string to uppercase 'a'.toUpperCase().
+//  For example. By default the string is 'lorem ipsum', the result will be 'Lorem Ipsum'.
 
-for (let i = 1; i < 11; i++) {
-  for (let j = 1; j < 11; j++) {
-    document.write(`${i * j}, `);
+const newString = "lorem ipsum dolor amen";
+
+let result;
+let pos = 0;
+
+while (pos !== -1) {
+  if (pos == 0) {
+    result = newString.substr(pos, 1).toUpperCase();
+    console.log(result);
+  } else {
+    result += newString.substr(pos + 1, 1).toUpperCase();
+    console.log(result + "   else");
   }
-  document.write(`<br>`);
+
+  let initialPos;
+
+  if (pos == 0) {
+    initialPos = pos;
+  } else {
+    initialPos = pos + 1;
+    console.log(initialPos + " initial");
+  }
+
+  pos = newString.indexOf(" ", pos + 1);
+  console.log(pos);
+
+  if (pos !== -1) result += newString.substring(initialPos + 1, pos + 1);
+  console.log(result);
+
+  if (initialPos > 0 && pos == -1) {
+    result += newString.substring(initialPos + 1, newString.stringLength);
+    document.write(result);
+  }
 }
