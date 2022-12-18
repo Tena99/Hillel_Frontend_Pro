319f3b1bb82b4a4349f3e77b742595bff5f5c7f6
Tena99 <stacyvolvis@gmail.com>
2022-12-18 22:07:46 +0200

    The fourth task done.

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-// Task 3.
-// Three numbers are entered. Determine which of them is the largest.
+// Task 4.
+// Three different numbers are entered. Find which one is the average (greater than one but less than the other).
 
 let first_number = prompt("Please enter the first number");
 let second_number = prompt("Please enter the second number");
@@ -14,12 +14,26 @@ if (
   third_number == ""
 ) {
   alert(`Unfortunately, you did not specify one or more numbers.`);
-} else if (first_number >= second_number && first_number >= third_number) {
-  alert(`Thank you! The highest number is ${first_number}`);
-} else if (second_number >= first_number && second_number >= third_number) {
-  alert(`Thank you! The highest number is ${second_number}`);
-} else if (third_number >= first_number && third_number >= second_number) {
-  alert(`Thank you! The highest number is ${third_number}`);
+} else if (first_number > second_number && first_number < third_number) {
+  alert(`Thank you! The average number is ${first_number}`);
+} else if (first_number < second_number && first_number > third_number) {
+  alert(`Thank you! The average number is ${first_number}`);
+} else if (second_number > first_number && second_number < third_number) {
+  alert(`Thank you! The average number is ${second_number}`);
+} else if (second_number < first_number && second_number > third_number) {
+  alert(`Thank you! The average number is ${second_number}`);
+} else if (third_number > first_number && third_number < second_number) {
+  alert(`Thank you! The average number is ${third_number}`);
+} else if (third_number < first_number && third_number > second_number) {
+  alert(`Thank you! The average number is ${third_number}`);
+} else if (first_number == second_number) {
+  alert(`Thank you! The average number is ${first_number}`);
+} else if (second_number == third_number) {
+  alert(`Thank you! The average number is ${second_number}`);
+} else if (third_number == first_number) {
+  alert(`Thank you! The average number is ${third_number}`);
+} else if (third_number < first_number && third_number > second_number) {
+  alert(`Thank you! The average number is ${third_number}`);
 } else {
   alert(`Please try again`);
 }
