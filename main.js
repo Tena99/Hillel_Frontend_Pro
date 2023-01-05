a069c4c795f189d89c81c6579316406085e7bb2e
Tena99 <stacyvolvis@gmail.com>
2023-01-05 18:29:21 +0200

    Task 13

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
-// Task 12. Сравнение строк без учёта регистра
-// Напишите функцию comparison(str1, str2), которая сравнивает строки без учёта регистра символов.
+// Task 13. Ограничить длину строки.
+// Напишите функцию limitStr(str, n, symb), которая обрезает строку, если она длиннее указанного количества символов n.
+// Усеченная строка должна заканчиваться троеточием «...» (если не задан параметр symb) или заданным символом symb.
 
-function compareString(str1, str2) {
-  str1 = str1.toLowerCase();
-  str2 = str2.toLowerCase();
-
-  if (str1 == str2) {
-    return true;
+function limitStr(str, n = 0, symb = null) {
+  if (n !== 0 && symb == null) {
+    str = str.substring(0, n);
+    return `${str}...`;
+  } else if (n !== 0 && symb !== null) {
+    str = str.substring(0, n);
+    return `${str}${symb}`;
   } else {
-    return false;
+    return str;
   }
 }
 
-console.log(compareString("string", "StRiNg")); // true
-console.log(compareString("ABCDe", "AbcdW")); // false
+console.log(limitStr("Каждый охотник желает знать.")); // Каждый охотник желает знать.
+console.log(limitStr("Каждый охотник желает знать.", 14)); // Каждый охотник...
+console.log(limitStr("Каждый охотник желает знать.", 21, "!")); // Каждый охотник желает!
