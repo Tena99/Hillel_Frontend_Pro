21ebf967bf897652cf4212c76d0948c5efc338bd
Tena99 <stacyvolvis@gmail.com>
2023-01-05 18:42:32 +0200

    Task 22

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
-// ДЗ 21. IsEmpty. Напишите функцию, которая делает поверхностную проверку объекта на пустоту.
+// Task 22. Напишите функцию, которая поверхностно сравнивает два объекта.
 
-const isEmpty = function (object) {
-  for (key in object) {
-    if (object[key] !== undefined) {
+const isEqual = function (firstObject, secondObject) {
+  for (let item in firstObject) {
+    let tmp = item + firstObject[item];
+    let tmp2 = item + secondObject[item];
+
+    console.log(`tmp1 - ${tmp}, tmp2 - ${tmp2}`);
+
+    if (tmp !== tmp2) {
       return false;
     }
   }
-
   return true;
 };
 
-const data = { a: 1, b: undefined };
-const data2 = { a: undefined };
-
-console.log(isEmpty(data)); // false
-console.log(isEmpty(data2)); // true
+const data = { a: 1, b: 1 };
+const data2 = { a: 1, b: 1 };
+const data3 = { a: 1, b: 2 };
+console.log(isEqual(data, data2)); // true
+console.log(isEqual(data, data3)); // false
